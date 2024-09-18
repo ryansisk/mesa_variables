9417d537b03c29aa03458fa4e8202d6237fcf98f
Ryan K Sisk <ryan.sisk@gmail.com>
2024-09-18 16:28:27 +0000

    using awesometable

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 from pathlib import Path
+from awesome_table import AwesomeTable
+from awesome_table.column import (Column, ColumnDType)
 
 # Load the data
 @st.cache_data
@@ -17,18 +19,8 @@ def load_data():
 
 df = load_data()
 
-# Custom CSS to adjust table width
-st.markdown(
-    """
-    <style>
-    .dataframe-container {
-        width: 100% !important;
-    }
-    </style>
-    """, unsafe_allow_html=True
-)
-
-st.title('MESA Variables from Main Exams')
+st.set_page_config(page_title='MESA Variable Name Description Table', layout='wide')
+#st.title('')
 
 # Sidebar for exam selection
 exams = df['Source File'].unique()
@@ -38,16 +30,24 @@ selected_exams = st.sidebar.multiselect('Select Exams', exams, default=exams)
 filtered_df = df[df['Source File'].isin(selected_exams)]
 
 # Search functionality
-search_term = st.text_input('Search for a variable name or description:')
+#search_term = st.text_input('Search for a variable name or description:')
 
-if search_term:
-    filtered_df = filtered_df[
-        filtered_df['Variable Name'].str.contains(search_term, case=False) |
-        filtered_df['Description'].str.contains(search_term, case=False)
-    ]
+#if search_term:
+#    filtered_df = filtered_df[
+#        filtered_df['Variable Name'].str.contains(search_term, case=False) |
+#        filtered_df['Description'].str.contains(search_term, case=False)
+#    ]
 
-# Display the filtered dataframe in a container with a custom class
-st.dataframe(filtered_df, use_container_width=True)
+# Display the filtered dataframe
+#st.dataframe(filtered_df, use_container_width=True)
+
+AwesomeTable(filtered_df, columns=[
+    Column(name='Variable Name', label='Variable ID'),
+    Column(name='Description', label='Variable Definition'),
+    Column(name='Type', label='Data Type'),
+    Column(name='Code=Value', label='Code Definitions'),
+    Column(name='Source file', label='Exam'),
+], show_search=True)
 
 # Display statistics
 st.sidebar.write(f"Total variables: {len(filtered_df)}")
